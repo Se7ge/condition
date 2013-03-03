b0f32272cc5c611037f967e093d1a47e891c023b
SAntipov <santipov@korusconsulting.ru>
2013-03-03 20:02:29 +0400

    Готовые решения

--- prepared/views.py
+++ prepared/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
-from condition.services.models import Services
+from condition.prepared.models import Prepared
 from django.shortcuts import render_to_response
 from django.template import Template, context, RequestContext
 
@@ -8,5 +8,5 @@ from django.template import Template, context, RequestContext
 def show_prepared(request, id):
     template_name = 'prepared/prepared.html'
     return render_to_response(template_name,
-                              {'prepared': Services.objects.get(pk=int(id))},
+                              {'prepared': Prepared.objects.get(pk=int(id))},
                               context_instance=RequestContext(request))
