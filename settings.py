14317ebb1bb16bfdda7953b7c1bf2494771e9e52
Serge <antipov.serge@gmail.com>
2013-05-07 16:45:14 +0400

    Product contact form

--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Django settings for condition project.
 import os.path
 
@@ -13,9 +14,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'nwclimatru',                      # Or path to database file if using sqlite3.
-        'USER': 'nwclimatru',                      # Not used with sqlite3.
-        'PASSWORD': 'condition_password',                  # Not used with sqlite3.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
         'OPTIONS': {
@@ -191,5 +192,14 @@ LOGGING = {
         },
     }
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'  # адрес smtp-сервера. например 'smtp.gmail.com' # # для локального хоста - 'localhost'
+EMAIL_PORT = 1025  # порт smtp-сервера (обычно 587 или 25 для TLS или 465 для SSL)
+# # для локального хоста - обычно порт 1025
+EMAIL_HOST_USER = ''  # логин
+EMAIL_HOST_PASSWORD = ''  # пароль
+EMAIL_USE_TLS = False  # включить/отключить TLS (для тестового режима - False)
+DEFAULT_FROM_EMAIL = 'no-reply@ex.ru'
 
 from settings_local import *
